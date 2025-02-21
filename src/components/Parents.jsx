8298056b93f90ac74adcc027b004d25d625cb431
Amy Jordan <amy.jordan@bt.com>
2025-02-21 10:16:52 +0000

    parents area layout

--- src/components/Parents.jsx
+++ src/components/Parents.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import Footer from "./footer.jsx";
 import {
   Main,
@@ -8,7 +9,16 @@ import {
   FormWrapper,
   WelcomeSection,
   Overlay,
+  Section,
+  Article,
+  FadeIn,
 } from "./StyledComponents.js";
+
+const ActivitiesOverview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+`;
 
 const Parents = () => {
   const [password, setPassword] = useState("");
@@ -53,17 +63,45 @@ const Parents = () => {
             <Button type="submit">Unlock Access</Button>
           </Form>
         ) : (
-          <div>
+          <Section>
             <h2>🎉 Welcome to the Parents Area!</h2>
             <p>
               You now have access to all the latest updates, event schedules,
               and resources tailored for you. Thanks for being part of the 5th
               Stowmarket Brownies family!
             </p>
-            <Footer />
-          </div>
+            <ActivitiesOverview>
+              <FadeIn>
+                <Article>
+                  <h3>New starter info</h3>
+                  <p>
+                    Here you can revisit all the info given out when your
+                    daughter first joins our unit.
+                  </p>
+                </Article>
+              </FadeIn>
+              <FadeIn>
+                <Article>
+                  <h3>Termly plans</h3>
+                  <p>
+                    You can download all our past termly letters which include
+                    the plans for that term.
+                  </p>
+                </Article>
+              </FadeIn>
+              <FadeIn>
+                <Article>
+                  <h3>Forms/letters</h3>
+                  <p>
+                    If you're missing a form or a letter, you can find it here.
+                  </p>
+                </Article>
+              </FadeIn>
+            </ActivitiesOverview>
+          </Section>
         )}
       </FormWrapper>
+      <Footer />
     </Main>
   );
 };
